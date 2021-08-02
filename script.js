80da408184c2ac5fcd6127caf6a57da30f08576d
Ninua-Jin <ni.jintcharadze@gmail.com>
2021-08-02 14:11:56 +0400

    add error text

--- script.js
+++ script.js
@@ -3,23 +3,32 @@ fetch('https://reqres.in/api/users?page=2', {
 })
 .then(function(response) {
     if (response.status !== 200) {
-        throw 'error';
+        throw response.status;
     }
     return response.json();
 })
 .then(function(data) {
     console.log(data);
     let ul = document.createElement('ul');
+    let fragment = document.createDocumentFragment();
 
     data.data.forEach(element => {
         let li = document.createElement('li');
         li.textContent = element.first_name;
-
-        ul.appendChild(li);
-        
+        fragment.appendChild(li)
     });
+
+    ul.appendChild(fragment);
     document.getElementById('api').appendChild(ul);
 })
 .catch(function(error) {
-    console.log(error)
+    if (error == 404) {
+        let p = document.createElement('p');
+        p.textContent = 'Page Not Found!';
+        document.getElementById('api').appendChild(p)
+    } else {
+        let p = document.createElement('p');
+        p.textContent = 'Server Error!';
+        document.getElementById('api').appendChild(p)
+    }
 })
